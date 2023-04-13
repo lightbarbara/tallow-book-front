8faa82d94eaf738d0aee7d39f62f9002f94d8d39
Barbara Luz <barbaraluz@eq.ufrj.br>
2023-04-12 21:32:51 -0300

    feat: my books page

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -1,3 +1,47 @@
+import { useContext, useState } from "react";
+import OutsideContainer from "../components/OutsideContainer";
+import PagesContainer from "../components/PagesContainer";
+import SideMenu from "../components/SideMenu";
+import TopBar from "../components/TopBar";
+import { UserContext } from "../contexts/UserContext";
+import axios from "axios";
+
 export default function MyBooks() {
-    
+
+    const [books, setBooks] = useState([])
+
+    const { token } = useContext(UserContext)
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    useState(() => {
+
+        const getBooks = async () => {
+            try {
+
+                const res = await axios.get(`${process.env.REACT_APP_BACK_END_URL}/my-books`, config)
+                setBooks(res.data.books)
+
+            } catch (err) {
+                alert(err.response.data.message)
+            }
+        }
+
+        getBooks()
+
+    }, [])
+
+    return (
+        <OutsideContainer>
+            <TopBar />
+            <PagesContainer books={books}>
+                <SideMenu />
+                {books.length === 0 ? <p>Você ainda não cadastrou nenhum livro na plataforma</p> : <BookList books={books} />}
+            </PagesContainer>
+        </OutsideContainer>
+    )
 }
